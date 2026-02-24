4522dc762351c264dbe44d6a58d387af6d7fcfb7
Dina Muravska <dina.muravska.25@gmail.com>
2026-02-24 23:36:53 +0200

    add folders components and css files

---
+++ src/App/App.tsx
@@ -0,0 +1,11 @@
+// import { useState } from "react";
+import CafeInfo from "../CafeInfo/CafeInfo";
+import css from "./App.module.css";
+
+export default function App() {
+  return (
+    <div className={css.app}>
+      <CafeInfo />
+    </div>
+  );
+}
