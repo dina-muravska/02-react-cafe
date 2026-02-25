8bde709292ec4caf7784cd7877779003d4371960
Dina Muravska <dina.muravska.25@gmail.com>
2026-02-25 23:28:53 +0200

    add interface and type for votes

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,8 +1,22 @@
-// import { useState } from "react";
+import { useState } from "react";
 import CafeInfo from "../CafeInfo/CafeInfo";
 import css from "./App.module.css";
-
+import { Votes, VoteType } from "../types/votes";
 export default function App() {
+  const [votes, setVotes] = useState<Votes>({
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  });
+  const handleVote = (type: VoteType) => {
+    setVotes((votes) => ({
+      ...votes,
+      [type]: votes[type] + 1,
+    }));
+  };
+  const resetVotes = () => {
+    setVotes({ good: 0, neutral: 0, bad: 0 });
+  };
   return (
     <div className={css.app}>
       <CafeInfo />
