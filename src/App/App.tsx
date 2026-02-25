2ea67bd2b28efdd94f212cdcbe6295308fb51136
Dina Muravska <dina.muravska.25@gmail.com>
2026-02-25 23:49:32 +0200

    add component VoteOptions

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,25 +1,36 @@
 import { useState } from "react";
 import CafeInfo from "../CafeInfo/CafeInfo";
 import css from "./App.module.css";
+import VoteOptions from "../VoteOptions/VoteOptions";
 import { Votes, VoteType } from "../types/votes";
+
 export default function App() {
   const [votes, setVotes] = useState<Votes>({
     good: 0,
     neutral: 0,
     bad: 0,
   });
+
   const handleVote = (type: VoteType) => {
     setVotes((votes) => ({
       ...votes,
       [type]: votes[type] + 1,
     }));
   };
+
+  const totalVotes = votes.good + votes.neutral + votes.bad;
+
   const resetVotes = () => {
     setVotes({ good: 0, neutral: 0, bad: 0 });
   };
   return (
     <div className={css.app}>
       <CafeInfo />
+      <VoteOptions
+        onVote={handleVote}
+        onReset={resetVotes}
+        canReset={totalVotes > 0}
+      />
     </div>
   );
 }
