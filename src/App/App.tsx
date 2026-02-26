0848e927a842afa6550b47fbf0f378ca2795edcc
Dina Muravska <dina.muravska.25@gmail.com>
2026-02-26 19:00:35 +0200

    add notification component

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -4,6 +4,7 @@ import css from "./App.module.css";
 import VoteOptions from "../VoteOptions/VoteOptions";
 import type { Votes, VoteType } from "../types/votes";
 import VoteStats from "../VoteStats/VoteStats";
+import Notification from "../Notification/Notification";
 
 export default function App() {
   const [votes, setVotes] = useState<Votes>({
@@ -25,7 +26,9 @@ export default function App() {
 
   const totalVotes = votes.good + votes.neutral + votes.bad;
 
-  const positiveRate = totalVotes ? Math.round((votes.good / totalVotes) * 100) : 0;
+  const positiveRate = totalVotes
+    ? Math.round((votes.good / totalVotes) * 100)
+    : 0;
 
   return (
     <div className={css.app}>
@@ -36,10 +39,15 @@ export default function App() {
         canReset={totalVotes > 0}
       />
 
-      {totalVotes > 0 ? (<VoteStats
-        votes={votes}
-        totalVotes={totalVotes}
-        positiveRate={positiveRate} />):}
+      {totalVotes > 0 ? (
+        <VoteStats
+          votes={votes}
+          totalVotes={totalVotes}
+          positiveRate={positiveRate}
+        />
+      ) : (
+        <Notification />
+      )}
     </div>
   );
 }
